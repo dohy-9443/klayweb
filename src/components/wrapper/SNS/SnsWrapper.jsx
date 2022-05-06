5eff9d70b60356082458c219a0f3edc9befe2862
YunSeok <cys9072@naver.com>
2022-05-06 17:16:34 +0900

    -🦖 sns 연결 링크 새로 오픈되게 수정

--- src/components/wrapper/SNS/SnsWrapper.jsx
+++ src/components/wrapper/SNS/SnsWrapper.jsx
@@ -6,19 +6,43 @@ const SnsWrapper = () => {
 
   return (
     <S.SnsUl>
-      <S.SNS onClick={() => navigate(`https://twitter.com/KlayLionsNFT`)}>
+      <S.SNS
+        onClick={() =>
+          //  navigate(`/https://twitter.com/KlayLionsNFT`)
+          {
+            window.open("https://twitter.com/KlayLionsNFT", "_blank");
+          }
+        }
+      >
         <img src="/assets/img/icon/Twitter.svg" alt="" />
       </S.SNS>
-      <S.SNS onClick={() => navigate(`https://discord.com/invite/DEKyBYtjU5`)}>
+      <S.SNS
+        onClick={() =>
+          // navigate(`/https://discord.com/invite/DEKyBYtjU5`)
+          {
+            window.open("https://discord.com/invite/DEKyBYtjU5", "_blank");
+          }
+        }
+      >
         <img src="/assets/img/icon/discord.svg" alt="" />
       </S.SNS>
       <S.SNS
-        onClick={() => navigate(`https://opensea.io/collection/klay-lions`)}
+        onClick={() =>
+          // navigate(`/https://opensea.io/collection/klay-lions`)
+          {
+            window.open("https://opensea.io/collection/klay-lions", "_blank");
+          }
+        }
       >
         <img src="/assets/img/icon/opensea.svg" alt="" />
       </S.SNS>
       <S.SNS
-        onClick={() => navigate(`https://lions-klay.gitbook.io/klay-lions`)}
+        onClick={() =>
+          // navigate(`/https://lions-klay.gitbook.io/klay-lions`)
+          {
+            window.open("https://lions-klay.gitbook.io/klay-lions", "_blank");
+          }
+        }
       >
         <img src="/assets/img/icon/gitbook.svg" alt="" />
       </S.SNS>
